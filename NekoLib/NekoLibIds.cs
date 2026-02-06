c2c4608183edd5151520afdc62879702aeec79c9
Nekovih <rafael.sakamoto1@hotmail.com>
2026-02-06 12:30:54 -0300

    Nekolib.Core died Nekolib project now is only metadata content

---
+++ NekoLib/NekoLibIds.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NekoLib
+{
+    public static class NekoLibIds
+    {
+        public const string Navigation = "NekoLib.Navigation";
+        public const string Devices = "NekoLib.Devices";
+        public const string Diagnostics = "NekoLib.Diagnostics";
+        public const string Data = "NekoLib.Data";
+        public const string Watchdog = "NekoLib.Runtime.Watchdog";
+    }
+}
+
